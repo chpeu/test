12e50a54b6f29b16d95dcbbf0d290a288f273b46
chpeu <129604005+chpeu@users.noreply.github.com>
2025-11-15 21:26:09 +0100

    Update postgresql_datalogger.py

--- core/postgresql_datalogger.py
+++ core/postgresql_datalogger.py
@@ -894,8 +894,8 @@ class PostgreSQLDataLogger:
                     %s, %s,
                     %s, %s, %s, %s,
                     %s, %s,
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
-                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
                 )
                 RETURNING id
             """
